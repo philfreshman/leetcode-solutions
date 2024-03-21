bd1b0dc2837ae89530468ceaa00abf023d917f06
philfreshman <philipswiezak@gmail.com>
2024-03-21 12:03:26 +0100

    add 0238 - product of array except self

---
+++ c#/0238_product_of_array_except_self.cs
@@ -0,0 +1,22 @@
+// time: O(n)
+// space O(n)
+
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+        var res = new int[nums.Length];
+
+        var prefix = 1;
+        for (int i = 0; i < nums.Length; i++) {
+            res[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        var postfix = 1;
+        for (int i = nums.Length - 1; i >= 0; i--) {
+            res[i] *= postfix;
+            postfix *= nums[i];
+        }
+
+        return res;
+    }
+}
