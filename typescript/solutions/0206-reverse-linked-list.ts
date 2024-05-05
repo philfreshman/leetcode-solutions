f4b18e8379b54565a0800874b99690195e975197
philfreshman <philipswiezak@gmail.com>
2024-05-05 21:42:17 +0200

    rearrange project files

---
+++ typescript/solutions/0206-reverse-linked-list.ts
@@ -0,0 +1,19 @@
+export class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val
+    this.next = next === undefined ? null : next
+  }
+}
+
+export function reverseList(head: ListNode | null): ListNode | null {
+  let newHead: ListNode | null = null
+
+  while (head !== null) {
+    newHead = new ListNode(head.val, newHead)
+    head = head.next
+  }
+
+  return newHead
+}
